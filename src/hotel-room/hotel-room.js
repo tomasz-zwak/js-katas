8c68c81bf70f31ea418cc8750f180417a2d36c1c
tomasz-zwak <t.zwak@selleo.com>
2021-12-16 15:39:58 +0100

    add readme

--- src/hotel-room/hotel-room.js
+++ src/hotel-room/hotel-room.js
@@ -17,25 +17,29 @@ function sumArrayNumbers(array) {
 
 //subSets with 2 or more elements
 function subSets(array) {
-  return array.reduce(
-    (subsets, value) => subsets.concat(subsets.map((set) => [value, ...set])),
-    [[]]
-  ).filter(subSet => subSet.length > 1);
+  return array
+    .reduce(
+      (subsets, value) => subsets.concat(subsets.map((set) => [value, ...set])),
+      [[]]
+    )
+    .filter((subSet) => subSet.length > 1);
 }
 
-function hotelRoom(roomsNumber = 100) {
-  let roomNo = 0;
+function hotelRoom(roomsNumber = 200) {
+  let roomNo = 100;
   while (roomNo <= roomsNumber) {
     const divisorArray = divisors(roomNo);
     const divisorSum = sumArrayNumbers(divisorArray);
     const subSetsSums = [];
-    const sSets = subSets(divisorArray)
+    const sSets = subSets(divisorArray);
     sSets.forEach((ss) => {
       subSetsSums.push(sumArrayNumbers(ss));
     });
-    if(roomNo < divisorSum && !subSetsSums.includes(roomNo)) return roomNo;
+    if (roomNo < divisorSum && !subSetsSums.includes(roomNo)) return roomNo;
     roomNo += 1;
   }
 }
+
+hotelRoom();
 
 module.exports = hotelRoom;
